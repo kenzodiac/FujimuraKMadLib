51ab41f4ba82775d35a20016bbbaaf0e7be17d17
aisha mashrah <aishamashrah@gmail.com>
2022-10-20 15:19:01 -0700

    commit

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 //10-20-2022
 //GitHub Challenge # 5 - Mad Lib
 //Console Program will guide user to complete a 'Mad Lib' story by soliciting words to fill in the blanks of the Mad Lib.
-//Peer Review by:
+// Peer Reviewed By: Aisha Mashrah
+// His work is perfect.Its good for Submission Guidelines.Its different then all the codes I have reviewed its really fun to play.The Ability to play again and the 
+// Data validation both work well.
 
 Console.Clear();
 
--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Last Revision: 10-20-2022
 GitHub Challenge #5: "Mad Lib"
 Console Program will guide user to complete a 'Mad Lib' story by soliciting words to fill in the blanks of the Mad Lib.
 
-Peer Reviewed By: 
+Peer Reviewed By: Aisha Mashrah
+His work is perfect.Its good for Submission Guidelines.Its different then all the codes I have reviewed its really fun to play.The Ability to play again and the 
+Data validation both work well.
+
 
