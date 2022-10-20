d3a0367aa2c58e352f77d4ad420751bbb69ce5c8
Kenzodiac <kennethfujimura@gmail.com>
2022-10-20 11:10:11 -0700

    Prep for peer review

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿//Kenneth Fujimura
-//10-18-2022
-//Mini Challenge # 5 - Mad Lib
+//10-20-2022
+//GitHub Challenge # 5 - Mad Lib
 //Console Program will guide user to complete a 'Mad Lib' story by soliciting words to fill in the blanks of the Mad Lib.
 //Peer Review by:
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # KFujimuraMadLib
+Program By: Kenneth Fujimura
+Last Revision: 10-20-2022
+GitHub Challenge #5: "Mad Lib"
+Console Program will guide user to complete a 'Mad Lib' story by soliciting words to fill in the blanks of the Mad Lib.
+
+Peer Reviewed By: 
+
